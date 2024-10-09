0d1f873fe0edd0e6d7f2deb07f6cd75d9c76be56
palva39 <paa39@njit.edu>
2024-10-09 15:43:26 -0400

    fixed pylint tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,3 +1,7 @@
+"""
+This module sets up pytest configuration, including dynamic test generation
+using Faker and providing custom test data for calculator tests.
+"""
 # conftest.py
 from decimal import Decimal
 import pytest
@@ -7,7 +11,12 @@ from calculator.operations import add, subtract, multiply, divide
 fake = Faker()
 
 def generate_test_data(num_records):
-    # Define operation mappings for both Calculator and Calculation tests
+    """
+    Generate test data with random numbers and operations.
+
+    :param num_records: Number of test records to generate
+    :yield: A tuple containing operands, operation name, operation function, and expected result
+    """
     operation_mappings = {
         'add': add,
         'subtract': subtract,
@@ -22,8 +31,8 @@ def generate_test_data(num_records):
         operation_name = fake.random_element(elements=list(operation_mappings.keys()))
         operation_func = operation_mappings[operation_name]
 
-        if operation_func == divide:
-            b = Decimal('1') if b == Decimal('0') else b
+        if operation_func == divide and b == Decimal('0'):
+            b = Decimal('1') # Avoid division by zero by setting b to 1
 
         try:
             if operation_func == divide and b == Decimal('0'):
@@ -36,11 +45,16 @@ def generate_test_data(num_records):
         yield a, b, operation_name, operation_func, expected
 
 def pytest_addoption(parser):
+    """
+    Add a command-line option to specify the number of records for test generation.
+    """
     parser.addoption("--num_records", action="store", default=5,
                      type=int, help="Number of test records to generate")
 
 def pytest_generate_tests(metafunc):
-    # Check if the test is expecting any of the dynamically generated fixtures
+    """
+    Generate dynamic tests based on the number of records specified through the --num_records option.
+    """
     if {"a", "b", "expected"}.intersection(set(metafunc.fixturenames)):
         num_records = metafunc.config.getoption("num_records")
         parameters = list(generate_test_data(num_records))
