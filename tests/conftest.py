f88b4ac752dd90f1261d4fff3ea13b2a00bfa63f
palva39 <paa39@njit.edu>
2024-10-09 15:39:59 -0400

    fixed pylint tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -17,11 +17,11 @@ def generate_test_data(num_records):
     # Generate test data
     for _ in range(num_records):
         a = Decimal(fake.random_number(digits=2))
-        b = Decimal(fake.random_number(digits=2)) if _ % 4 != 3 else Decimal(fake.random_number(digits=1))
+        b = Decimal(fake.random_number(digits=2)) if _ % 4 != 3 else (
+            Decimal(fake.random_number(digits=1)))
         operation_name = fake.random_element(elements=list(operation_mappings.keys()))
         operation_func = operation_mappings[operation_name]
         
-        # Ensure b is not zero for divide operation to prevent division by zero in expected calculation
         if operation_func == divide:
             b = Decimal('1') if b == Decimal('0') else b
         
@@ -36,16 +36,15 @@ def generate_test_data(num_records):
         yield a, b, operation_name, operation_func, expected
 
 def pytest_addoption(parser):
-    parser.addoption("--num_records", action="store", default=5, type=int, help="Number of test records to generate")
+    parser.addoption("--num_records", action="store", default=5,
+                     type=int, help="Number of test records to generate")
 
 def pytest_generate_tests(metafunc):
     # Check if the test is expecting any of the dynamically generated fixtures
     if {"a", "b", "expected"}.intersection(set(metafunc.fixturenames)):
         num_records = metafunc.config.getoption("num_records")
-        # Adjust the parameterization to include both operation_name and operation for broad compatibility
-        # Ensure 'operation_name' is used for identifying the operation in Calculator class tests
-        # 'operation' (function reference) is used for Calculation class tests.
         parameters = list(generate_test_data(num_records))
         # Modify parameters to fit test functions' expectations
-        modified_parameters = [(a, b, op_name if 'operation_name' in metafunc.fixturenames else op_func, expected) for a, b, op_name, op_func, expected in parameters]
+        modified_parameters = [(a, b, op_name if 'operation_name' in metafunc.fixturenames else op_func, expected)
+                               for a, b, op_name, op_func, expected in parameters]
         metafunc.parametrize("a,b,operation,expected", modified_parameters)
